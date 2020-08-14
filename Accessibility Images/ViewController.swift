237735b9e2e465a047bba87970228232dc6fcd3c
Daniel Tull <dt@danieltull.co.uk>
2020-08-14 11:14:45 +0100

    Add images from wikipedia and add accesibility labels to some images

---
+++ Accessibility Images/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accessibility Images
+//
+//  Created by Daniel Tull on 14.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
